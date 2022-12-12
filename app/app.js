88a6d9e35a49ed419c0261035c784d7c3b856e23
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-12-12 19:53:12 -0300

    estudo de filter e  ng-include

--- app/app.js
+++ app/app.js
@@ -16,6 +16,21 @@ myFirstApp.controller('MyFirstAppController', ['$scope',($scope)=>{
     //todo código que será disparado quando o controller for chamado
 
     $scope.message= "Olá Mundo!"
-    $scope.nomes = ['João', 'Maria', 'José']
+    $scope.nomes = [
+        {
+        nome: "João",
+        cinto: "Preto",
+        preco: 100
+    },
+    {
+        nome: "Maria",
+        cinto: "Branco",
+        preco: 50
+    },
+    {
+        nome: "José",
+        cinto: "Azul",
+        preco: 80
+    }]
     
 }]) 
