ff09bbdcf06b7968a7bb78fb46fb2aae551f3104
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-12-14 09:22:46 -0300

    feat: implementa rotas utilizando ng-route

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
 
-const myFirstApp = angular.module('myFirstApp',[])//[] é um array de dependências
+const myFirstApp = angular.module('myFirstApp',['ngRoute'])//[] é um array de dependências
 
-myFirstApp.config(()=>{
+//todo código que será disparado antes de iniciar a aplicação
+myFirstApp.config(['$routeProvider', ($routeProvider)=>{
 
-    //todo código que será disparado antes de iniciar a aplicação
-
-})
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'MyFirstAppController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+    }
+])
 
 myFirstApp.run(()=>{
 
